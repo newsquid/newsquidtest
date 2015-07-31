38c005b0fd9afc210fd02d826cca5f8cdd8ad70f
Bladtman <sigurt.bladt@gmail.com>
2015-07-31 20:27:43 +0200

    toy backend for ass1

---
+++ backend/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/cors"
+	"github.com/newsquid/mscore"
+	"github.com/newsquid/newsquidtest/backend/api"
+)
+
+/*
+Application entry point
+*/
+func main() {
+	m := martini.New()
+
+	//Set middleware
+	m.Use(martini.Recovery())
+	m.Use(martini.Logger())
+
+	m.Use(cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"OPTIONS", "HEAD", "POST", "GET", "PUT"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	//Set returnhandler
+
+	//Create the router
+	r := martini.NewRouter()
+
+	//Options matches all and sends okay
+	r.Options(".*", func() (int, string) {
+		return 200, "ok"
+	})
+
+	api.SetupTodoRoutes(r)
+
+	mscore.StartServer(m, r)
+	fmt.Println("Started NSQ Test service")
+}
